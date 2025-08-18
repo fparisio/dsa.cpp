83d249863e005a93f645dbe79ea9cebd27ea30f6
Francesco Parisio <francesco.parisio@protonmail.com>
2025-08-18 15:30:33 +0200

    Assignement 1, ex 4, pyramid pattern

---
+++ Assignment1/4.cpp
@@ -0,0 +1,41 @@
+// Copyright 2025 Francesco Parisio
+
+// Pattern printing
+#include <iostream>
+#include <vector>
+
+void printLine(std::vector<int> &toprint) {
+  // Print the current line of the pattern
+  for (auto it = toprint.begin(); it != toprint.end(); ++it)
+    std::cout << *it << " ";
+  std::cout << std::endl;
+}
+
+void printPattern(std::vector<int> toprint) {
+  // Print the pattern in reverse order
+  while (toprint.begin() < toprint.end()) {
+    printLine(toprint);
+    toprint.pop_back();
+  }
+}
+
+int getNum() {
+  // Get an integer from user input
+  int num;
+  std::cout << "Type an integer: " << std::endl;
+  std::cin >> num;
+  return num;
+}
+
+std::vector<int> assembleVector(int num) {
+  std::vector<int> v1;
+  for (int i = 1; i <= num; i++) {
+    v1.push_back(i);
+  }
+  return v1;
+}
+
+int main() {
+  printPattern(assembleVector(getNum()));
+  return 0;
+}
